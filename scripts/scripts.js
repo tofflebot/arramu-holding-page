b291be215cd778f8bd84af1c14b8a76d844521fa
Tom <holt.thomas@gmail.com>
2020-08-10 11:56:43 +0100

    Updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,27 @@
-const navOpenButton = document.querySelector('.mobile-nav-open-button');
-const navCloseButton = document.querySelector('.mobile-nav-close-button');
-const nav = document.querySelector('nav');
-const navBackground = document.querySelector('.nav-background');
+// const navOpenButton = document.querySelector('.mobile-nav-open-button');
+// const navCloseButton = document.querySelector('.mobile-nav-close-button');
+// const nav = document.querySelector('nav');
+// const navBackground = document.querySelector('.nav-background');
 
 
-function openMobileNav() {
-    nav.classList.add('nav-open');
-    navBackground.classList.add('nav-open');
-}
+// function openMobileNav() {
+//     nav.classList.add('nav-open');
+//     navBackground.classList.add('nav-open');
+// }
 
 
-function closeMobileNav() {
-    nav.classList.remove('nav-open');
-    navBackground.classList.remove('nav-open');
-}
+// function closeMobileNav() {
+//     nav.classList.remove('nav-open');
+//     navBackground.classList.remove('nav-open');
+// }
 
-navOpenButton.addEventListener('click', openMobileNav);
-navCloseButton.addEventListener('click', closeMobileNav);
-navBackground.addEventListener('click', function(e) {
-    if (e.target === e.currentTarget) {
-        closeMobileNav();
-    }
-});
+// navOpenButton.addEventListener('click', openMobileNav);
+// navCloseButton.addEventListener('click', closeMobileNav);
+// navBackground.addEventListener('click', function(e) {
+//     if (e.target === e.currentTarget) {
+//         closeMobileNav();
+//     }
+// });
 
 function headerBuffer() {
     const headerBuffer = document.querySelector('.header-buffer');
